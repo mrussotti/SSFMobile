8a68cb18c7afb704c798a584de697645817d2e41
mrussotti <matthewlax24@yahoo.com>
2023-05-04 13:40:29 -0400

    users can sign in, sign up, and log out

---
+++ screens/login.js
@@ -0,0 +1,127 @@
+import React, {useState, useEffect} from 'react';
+import { TextInput } from 'react-native';
+import { StyleSheet, View, Text, KeyboardAvoidingView, TouchableOpacity } from 'react-native';
+import {auth} from '../firebase';
+
+const Login = ({ navigation }) => {
+    const [email,setEmail]=useState('')
+    const [password,setPassword]=useState('')
+
+
+    useEffect(() => {
+       const unsubscribe = auth.onAuthStateChanged(user => {
+            if (user){
+                navigation.navigate("Training")
+            }
+        })
+        return unsubscribe
+    }, [])
+
+    const handleSignUp = () => {
+        auth
+        .createUserWithEmailAndPassword(email, password)
+        .then (userCredentials => {
+            const user = userCredentials.user;
+            console.log("Registered in with:",user.email);
+        })
+        .catch(error => alert(error.message))
+    }
+
+    const handleLogin = () => {
+        auth
+        .signInWithEmailAndPassword(email, password)
+        .then (userCredentials => {
+            const user = userCredentials.user;
+            console.log("Logged in with:", user.email);
+        })
+        .catch(error => alert(error.message))
+    }
+
+
+    return (
+        <KeyboardAvoidingView
+            style={styles.container}
+            behavior='padding'
+        >
+            <View style={styles.inputContainer}>
+                <TextInput
+                    placeholder='Email'
+                    value={ email}
+                    onChangeText={text => setEmail(text)}
+                    style={styles.input}
+                />
+                <TextInput
+                    placeholder='Password'
+                    value={ password}
+                    onChangeText={text => setPassword(text)}
+                    style={styles.input}
+                    secureTextEntry
+                />
+            </View>
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity
+                    onPress={handleLogin}
+                    style={styles.button}>
+                    <Text style={styles.buttonText}>Login</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    onPress={handleSignUp}
+                    style={[styles.button, styles.buttonOutline]}>
+                    <Text style={styles.buttonOutlineText}>Sign Up</Text>
+                </TouchableOpacity>
+            </View>
+
+        </KeyboardAvoidingView>
+    );
+};
+
+export default Login;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    inputContainer: {
+        width: '80%',
+    },
+    input: {
+        backgroundColor: "white",
+        paddingHorizontal: 15,
+        paddingVertical: 10,
+        borderRadius: 10,
+        marginTop: 5,
+    },
+    buttonContainer: {
+        width: "60%",
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 40,
+    },
+    button: {
+        backgroundColor: "#0782F9",
+        width: '100%',
+        padding: 15,
+        borderRadius:10,
+        alignItems: 'center',
+    },
+    buttonOutline: {
+        backgroundColor: 'white',
+        marginTop: 5,
+        borderColor: '#0782F9',
+        borderWidth: 2,
+    },
+    buttonText:{
+        color: 'white',
+        fontWeight:'700',
+        fontSize: 16,
+    },
+    buttonOutlineText: {
+        color: '#0782F9',
+        fontWeight: '700',
+        fontSize: 16,
+    },
+});

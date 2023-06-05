e3106abdc9cf3295516d54e2955e6e499b913093
mrussotti <matthewlax24@yahoo.com>
2023-06-04 21:57:25 -0400

    added basic calorie tracker

--- screens/Recovery.js
+++ screens/Recovery.js
@@ -2,12 +2,16 @@
 import React from 'react';
 import { View, Text, Button } from 'react-native';
 import SleepTracker from '../components/SleepTracker';
+import CalorieTracker from '../components/CalorieTracker';
+
 
 const Recovery = ({ navigation }) => {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Recovery</Text>
       <SleepTracker />
+      <CalorieTracker />
+
       <Button
         title="Go back to Training"
         onPress={() => navigation.navigate('Training')}

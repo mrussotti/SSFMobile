34a50d98c984a5dd4ed11d3068988bde153f03cb
mrussotti <matthewlax24@yahoo.com>
2023-06-13 21:47:06 -0400

    added personal data and basic customization to sleep and calorie tracker

--- screens/Settings.js
+++ screens/Settings.js
@@ -1,11 +1,17 @@
 //settings.js
-import React from 'react';
-import { TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { TouchableOpacity, TextInput } from 'react-native';
 import { View, Text, Button, StyleSheet } from 'react-native';
 import { auth } from '../firebase';
 import { useNavigation } from '@react-navigation/native';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
 
 const Settings = ({ navigation }) => {
+  const [weight, setWeight] = useState('');
+  const [height, setHeight] = useState('');
+  const [age, setAge] = useState('');
+  const [editMode, setEditMode] = useState(false);
 
   const navigation2 = useNavigation();
   const handleSignOut = () => {
@@ -17,6 +23,36 @@ const Settings = ({ navigation }) => {
       .catch(error => alert(error.message))
   }
 
+  const handleSubmit = async () => {
+    const userId = auth.currentUser.uid;
+    try {
+      await firebase.firestore().collection('users').doc(userId).set({
+        weight,
+        height,
+        age,
+      }, { merge: true });
+      alert('Health data saved successfully!');
+      setEditMode(false);
+    } catch (error) {
+      alert('Error saving health data:', error);
+    }
+  }
+
+  useEffect(() => {
+    const userId = auth.currentUser.uid;
+    firebase.firestore().collection('users').doc(userId).get()
+      .then((doc) => {
+        if (doc.exists) {
+          setWeight(doc.data().weight);
+          setHeight(doc.data().height);
+          setAge(doc.data().age);
+        }
+      })
+      .catch((error) => {
+        console.log("Error getting document:", error);
+      });
+  }, []);
+
   return (
     <>
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
@@ -26,6 +62,45 @@ const Settings = ({ navigation }) => {
 
     <View style={styles.container}>
     <Text> Email: {auth.currentUser?.email} </Text>
+
+    {editMode ? (
+      <>
+        <TextInput
+          placeholder='Weight (lbs)'
+          value={ weight }
+          onChangeText={text => setWeight(text)}
+          style={styles.input}
+        />
+
+        <TextInput
+          placeholder='Height (inches)'
+          value={ height }
+          onChangeText={text => setHeight(text)}
+          style={styles.input}
+        />
+
+        <TextInput
+          placeholder='Age'
+          value={ age }
+          onChangeText={text => setAge(text)}
+          style={styles.input}
+        />
+
+        <TouchableOpacity onPress= {handleSubmit} style= {styles.button}>
+          <Text style= {styles.buttonText}> Submit</Text>
+        </TouchableOpacity>
+      </>
+    ) : (
+      <>
+        <Text>Weight: {weight} lbs</Text>
+        <Text>Height: {height} inches</Text>
+        <Text>Age: {age}</Text>
+        <TouchableOpacity onPress={() => setEditMode(true)} style= {styles.button}>
+          <Text style= {styles.buttonText}> Edit</Text>
+        </TouchableOpacity>
+      </>
+    )}
+
     <TouchableOpacity onPress= {handleSignOut} style= {styles.button}>
       <Text style= {styles.buttonText}> Sign Out</Text>
     </TouchableOpacity>
